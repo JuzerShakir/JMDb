12d66a02216f4248af22741644c1249a8e0dc61d
JuzerShakir <juz.shakir.software@gmail.com>
2021-03-11 09:26:56 +0530

    init Movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 JMDb::Application.routes.draw do
   root 'static_pages#home'
+
+  match '/movies', to: 'movies#home', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
