36d8c48893be7702a917bb4eac5c5f6bfb3eb9f1
JuzerShakir <juz.shakir.software@gmail.com>
2021-03-11 08:07:46 +0530

    Gemfile install & update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JMDb::Application.config.secret_key_base = '84c453258b990c903c54c678e7e36476d666735f83616e7cdc00ade2b28c34ee5397d66b86ddd6e7d801183f87d27c24c07c27446ba17255f6d3bd398b754180'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JMDb::Application.config.session_store :cookie_store, key: '_JMDb_session'
